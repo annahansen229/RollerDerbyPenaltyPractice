d5f1c2f1d76fcebf747a09b27fbd153f01c77d52
Anna Hansen <annahansen229@gmail.com>
2026-05-17 14:30:38 -0700

    start data file

---
+++ src/data/data.py
@@ -0,0 +1,39 @@
+TOPICS = {
+    "jammer": [
+        "jam ending",
+        "lead jammer",
+        "no earned pass",
+        "not lead jammer",
+        "star pass complete"
+    ],
+    "other": [
+        "expulsion",
+        "go to your bench",
+        "official review",
+        "official timeout",
+        "return to the track",
+        "team timeout"
+    ],
+    "pack": [
+        "no pack",
+        "out of play",
+        "pack is here"
+    ],
+    "penalties": {
+        "back block",
+        "cut",
+        "direction",
+        "forearm",
+        "head block",
+        "high block"
+        "illegal contact",
+        "interference",
+        "illegal position",
+        "leg block",
+        "low block",
+        "misconduct",
+        "multiplayer",
+        "report to the box",
+        "two penalties"
+    }
+}
